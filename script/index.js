f4e3231de807334779877760debcd7f321a9e88d
Révész Márton <revesz.marton@pomscloud.ie>
2025-07-26 23:24:28 +0200

    feat: add wake up andpoint call on loat; add typewrite

--- script/index.js
+++ script/index.js
@@ -1,44 +1,92 @@
-const form = document.getElementById("chatForm");
-const input = document.getElementById("questionInput");
+const form = document.getElementById('chatForm');
+const input = document.getElementById('questionInput');
 
-const baseUrl = "https://portfolio-backend-k1ed.onrender.com";
+// const baseUrl = "https://portfolio-backend-k1ed.onrender.com";
+const baseUrl = 'http://localhost:3000';
 
 var messages = [
     {
-        role: "system",
-        content: "Mindig rövid, tömör, lényegretörő választ adj!",
+        role: 'system',
+        content: 'Mindig rövid, tömör, lényegretörő választ adj!',
+    },
+    {
+        role: 'assistant',
+        content:
+            'Szia! Én én egy AI chatbot vagyok akinek megvan minden tanítva Marciról! Ha mármi kérdésed van róla, akkor tedd fel és én válaszolok!',
     },
 ];
 
-form.addEventListener("submit", async function (e) {
+// Run on page loading to wake up the backend
+(async () => {
+    try {
+        const response = await fetch(`${baseUrl}/wake`, {
+            method: 'GET',
+        });
+
+        const data = await response.json();
+        const serverMessage = data.message;
+        console.log(serverMessage);
+
+        if (serverMessage === 'Server is awake.') {
+            addAiMessage(messages[1].content, 'ai');
+        }
+    } catch (error) {
+        console.log(`Error on wake up: ${error}`);
+    }
+})();
+
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const value = input.value;
-    messages.push({ role: "user", content: value });
+    messages.push({ role: 'user', content: value });
 
-    console.log("Sent messages:", messages);
-    addMessage(value, "user");
+    console.log('Sent messages:', messages);
+    addUserMessage(value, 'user');
 
     try {
         const response = await fetch(`${baseUrl}/ask`, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(messages),
         });
 
         const data = await response.json();
-        messages.push({ role: "assistant", content: data.answer });
+        messages.push({ role: 'assistant', content: data.answer });
 
-        console.log("Received messages:", messages);
-        addMessage(data.answer, "ai");
+        console.log('Received messages:', messages);
+        addAiMessage(data.answer, 'ai');
     } catch (error) {
-        console.error("Hiba történt:", error);
+        console.error('Hiba történt:', error);
     }
 });
 
-function addMessage(message, side) {
+function addUserMessage(message) {
     document.getElementById(
-        "chat"
-    ).innerHTML += `<div class="${side}"><p>${message}</p></div>`;
+        'chat'
+    ).innerHTML += `<div class="user"><p>${message}</p></div>`;
+}
+
+function addAiMessage(message) {
+    const typeSpeed = 30;
+
+    const chat = document.getElementById('chat');
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'ai';
+
+    const p = document.createElement('p');
+    messageDiv.appendChild(p);
+    chat.appendChild(messageDiv);
+
+    let i = 0;
+    function type() {
+        if (i < message.length) {
+            p.textContent += message.charAt(i);
+            i++;
+            setTimeout(type, typeSpeed);
+        }
+    }
+
+    type();
 }
